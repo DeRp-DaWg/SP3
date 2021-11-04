aec2a38e33a08bc448742e46472b9882067ebe2a
notebrr <90389727+notebrr@users.noreply.github.com>
2021-11-04 09:50:59 +0100

    boi

--- src/Team.java
+++ src/Team.java
@@ -1,4 +1,11 @@
+import java.util.ArrayList;
+
 public class Team {
     private String teamName;
     private Player[] players;
+    private ArrayList<String> player = new ArrayList<>();
+
+    public Team(){
+        player.add("Abdi");
+    }
 }
