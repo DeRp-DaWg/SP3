3e682c31f9a861ce58c79ae2d3c0f856d920e204
Lars Peter Jørgensen <lars-games@outlook.com>
2021-11-04 11:44:04 +0100

    Removed ui object from Main

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,8 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        UI ui = new UI();
-        Team team1 = new Team("Team 1", ui.createPlayers("Player 1", "Player 2"));
-        Team team2 = new Team("Team 2", ui.createPlayers("Player 3", "Player 4"));
+        Team team1 = new Team("Team 1", UI.createPlayers("Player 1", "Player 2"));
+        Team team2 = new Team("Team 2", UI.createPlayers("Player 3", "Player 4"));
         Team[] teams = {team1,team2};
         Tournament tournament = new GroupTournament("Tournament 1",teams);
     }
