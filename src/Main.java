88b599f2c139acaed84c159e56bdb48179c661b2
Lars Peter Jørgensen <lars-games@outlook.com>
2021-11-04 11:42:20 +0100

    createPlayers in UI

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        String[] players1 = {"Player 1", "Player 2"};
-        String[] players2 = {"Player 3", "Player 4"};
-        Team team1 = new Team("Team 1", players1);
-        Team team2 = new Team("Team 2", players2);
+        UI ui = new UI();
+        Team team1 = new Team("Team 1", ui.createPlayers("Player 1", "Player 2"));
+        Team team2 = new Team("Team 2", ui.createPlayers("Player 3", "Player 4"));
         Team[] teams = {team1,team2};
         Tournament tournament = new GroupTournament("Tournament 1",teams);
     }
+
+
 }
--- src/UI.java
+++ src/UI.java
@@ -1,6 +1,16 @@
+import java.util.ArrayList;
+
 public class UI {
     public UI(){
         String[] players = {"Rehman", "Jens"};
         //Team team = new Team("TeamA", players);
+    }
+
+    public static ArrayList<Player> createPlayers(String... playerNames) {
+        ArrayList<Player> players = new ArrayList<>();
+        for (String playerName : playerNames) {
+            players.add(new Player(playerName));
+        }
+        return players;
     }
 }
