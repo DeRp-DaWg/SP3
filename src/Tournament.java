adc8832c682ba74294c32a184bfa05a6929723a3
Wolfgang1235 <cph-af167@cphbusiness.dk>
2021-11-04 11:11:49 +0100

    Lavet af Andreas

--- src/Tournament.java
+++ src/Tournament.java
@@ -1,5 +1,26 @@
-public abstract class Tournament {
-    public Tournament(){
+import java.util.Arrays;
 
+public abstract class Tournament {
+    public String tournamentName;
+    public Team[] teams;
+    public Match[] matches;
+
+    public Tournament(String tournamentName, Team[] teams, Match[] matches) {
+        this.tournamentName = tournamentName;
+        this.teams = teams;
+        this.matches = matches;
+    }
+
+    public abstract ArrangeMatches(){
+
+    }
+
+    @Override
+    public String toString() {
+        return "Tournament{" +
+                "tournamentName='" + tournamentName + '\'' +
+                ", teams=" + Arrays.toString(teams) +
+                ", matches=" + Arrays.toString(matches) +
+                '}';
     }
 }
