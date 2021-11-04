77b2d703db4599fd52d95630dddbb9acc432d8ab
notebrr <90389727+notebrr@users.noreply.github.com>
2021-11-04 11:01:32 +0100

    boi

--- src/KnockoutTournament.java
+++ src/KnockoutTournament.java
@@ -1,5 +1,6 @@
 public class KnockoutTournament extends Tournament {
     public KnockoutTournament(){
-
+        String[] players = {"Rehman", "Jens"};
+        Team team = new Team("DataHoldet", players);
     }
 }
