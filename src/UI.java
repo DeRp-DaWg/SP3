59e30c9165ce109ad313b896ab47b0e7cdd6e63d
notebrr <90389727+notebrr@users.noreply.github.com>
2021-11-04 11:26:34 +0100

    f

--- src/UI.java
+++ src/UI.java
@@ -1,6 +1,6 @@
 public class UI {
     public UI(){
         String[] players = {"Rehman", "Jens"};
-        Team team = new Team("DataHoldet", players);
+        //Team team = new Team("TeamA", players);
     }
 }
