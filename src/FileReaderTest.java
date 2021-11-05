32ceeec6f2ccd99a55df719146588add8b1db8c7
Lars Peter Jørgensen <lars-games@outlook.com>
2021-11-05 12:15:29 +0100

    IO readFromFile() in progress and changed the formatting of TournamentTestFile.csv again.

--- src/FileReaderTest.java
+++ src/FileReaderTest.java
@@ -1,5 +1,13 @@
+import java.io.IOException;
+
 public class FileReaderTest {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        try {
+            IO io = new IO("TournamentTestFile.csv");
+            io.readFromFile();
+        }
+        catch (IOException e) {
+            System.out.println(e);
+        }
     }
 }
--- src/IO.java
+++ src/IO.java
@@ -6,24 +6,52 @@ import java.util.Scanner;
 
 public class IO {
     private String getData = "";
-    private String fileName = "tournament.csv";
-    private File myFile = new File(fileName);
     private FileWriter writeFile;
+    private File myFile;
+    private Scanner sc;
 
-    public IO() throws IOException {
+    public IO(String fileName) throws IOException {
+        File myFile = new File("Resources/"+fileName);
         if(!myFile.exists()){
             myFile.createNewFile();
         }
 
         writeFile = new FileWriter(fileName);
-        Scanner sc = new Scanner(myFile);
-        while (sc.hasNextLine()){
-            getData = sc.nextLine() + getData;
-        }
+        sc = new Scanner(myFile);
+        //while (sc.hasNextLine()){
+        //    getData = sc.nextLine() + getData;
+        //}
     }
 
     public void addToFile(String teamName, String[] players) throws IOException {
         writeFile.write(getData + "\n" + teamName + ", " + players);  //TILFØJ MERE
         writeFile.close();
+    }
+
+    public void readFromFile() {
+        String[] lines = new String[4];
+        int count = 0;
+        while (sc.hasNextLine()){
+            lines[count] = sc.nextLine();
+            count++;
+        }
+        //for (String line : lines) {
+        //    System.out.println(line);
+        //}
+        String[][] text = new String[lines.length][];
+        for (int i = 0; i < lines.length; i++) {
+            text[i] = lines[i].split(",");
+        }
+        for (String[] string : text) {
+            String teamName = string[0];
+            String[] teamMembers = string[1].split("\\|");
+            String[] matchText = string[2].split("\\|");
+            for (String match : matchText) {
+                String[] test = match.split("\\.");
+            }
+            for (int i = 0; i < matchText.length; i++) {
+                System.out.print(matchText[i]);
+            }
+        }
     }
 }
