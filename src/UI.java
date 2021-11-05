9ba272d8493aad702aee081fd443d688c5f3edc9
notebrr <90389727+notebrr@users.noreply.github.com>
2021-11-05 10:16:39 +0100

    f

--- src/UI.java
+++ src/UI.java
@@ -1,15 +1,30 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class UI {
+    String teamName = "";
+    Scanner sc;
+
     public UI(){
         String[] players = {"Rehman", "Jens"};
         //Team team = new Team("TeamA", players);
     }
 
-    public static ArrayList<Player> createPlayers(String... playerNames) {
+    public ArrayList<Player> createPlayers(String... playerNames){
+        sc = new Scanner(System.in);
+
+        //Get teamname
+        System.out.println("Holdnavn: ");
+        teamName = sc.nextLine();
+
+        //Get players
+        System.out.println("Indtast spillernavne separeret af et komma, fx Ole, Abdi, Hans:");
+
+
         ArrayList<Player> players = new ArrayList<>();
         for (String playerName : playerNames) {
-            players.add(new Player(playerName));
+            Player player = new Player(playerName);
+            players.add(player);
         }
         return players;
     }
