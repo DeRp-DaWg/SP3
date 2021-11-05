d6aaaa0a0a97aa36941b81902d132aac55be7431
jonathanbraad <jonathanbraad123@gmail.com>
2021-11-05 12:24:35 +0100

    b

--- src/GroupTournament.java
+++ src/GroupTournament.java
@@ -1,6 +1,34 @@
 public class GroupTournament extends Tournament{
-    public GroupTournament(String tournamentName, Team[] teams) {
+
+    public GroupTournament(String tournamentName, Team[] teams, Match[] matches)
+    {
         super(tournamentName, teams);
+    }
+
+    public int winnerTeamNewTotalScore(int winnerTeamCurrentScore) {
+        return winnerTeamCurrentScore + 2;
+    }
+
+    // For use in determineLoserTeamOutcome() method
+    public int loserTeamGoalScore(int winnerTeamMatchScore, int loserTeamMatchScore) {
+        int goalScore = winnerTeamMatchScore - loserTeamMatchScore;
+        return goalScore;
+    }
+
+    public void determineWinnerTeamOutcome(boolean team1Won, int team1CurrentScore, int team2CurrentScore) {
+        if (team1Won) {
+            winnerTeamNewTotalScore(team1CurrentScore);
+        } else {
+            winnerTeamNewTotalScore(team2CurrentScore);
+        }
+    }
+
+    public void determineLoserTeamOutcome(boolean team2Lost, int team1GoalScore, int team2GoalScore) {
+        if (team2Lost) {
+            loserTeamGoalScore(team1GoalScore,team2GoalScore);
+        } else {
+            loserTeamGoalScore(team2GoalScore,team1GoalScore);
+        }
     }
 
     @Override
