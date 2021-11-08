d3bae76c6dea310891022f75d33d55e346883e64
Wolfgang1235 <cph-af167@cphbusiness.dk>
2021-11-08 10:33:36 +0100

    Lavet af Andreas

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,16 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Team team1 = new Team("Team 1");
-        Team team2 = new Team("Team 2");
+        Team team1 = new Team("Team 1",0,0,false);
+        Team team2 = new Team("Team 2",0,0,false);
         Team[] teams = {team1,team2};
+
+
+
+        Match match1 = new Match(teams);
+        Match[] matches = {match1};
+
+        Tournament tournament = new GroupTournament("Tournament 1",teams,matches);
+
     }
 }
