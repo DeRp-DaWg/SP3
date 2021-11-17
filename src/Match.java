414d82096ff353be4ee03ff7b367437f790739c6
Wolfgang1235 <cph-af167@cphbusiness.dk>
2021-11-17 11:59:06 +0100

    Lavet af Andreas

--- src/Match.java
+++ src/Match.java
@@ -2,13 +2,20 @@ import java.util.ArrayList;
 
 public class Match {
     private Team[] teams;
-    private int matchGoalScore;
     private String matchName;
 
     ArrayList<String> myMatch = new ArrayList<>();
 
-    public Match(Team[] teams) {
+    public Match(Team[] teams, String matchName) {
         this.teams = teams;
+        this.matchName = matchName;
     }
 
+    public Team[] getTeams() {
+        return teams;
+    }
+
+    public String getMatchName() {
+        return matchName;
+    }
 }
