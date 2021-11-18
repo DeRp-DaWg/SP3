42f4b6123a0b56f1cb01e9174630f0e645237aab
notebrr <90389727+notebrr@users.noreply.github.com>
2021-11-18 22:16:31 +0100

    k

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException, SQLException {
-        //Team team1 = new Team("Team 1",0,0,false);
-        //Team team2 = new Team("Team 2",0,0,false);
-
         UI ui = new UI();
         ui.start();
-
-        IO io = new IO();
-
-        //Match match1 = new Match(teams);
-        //Match[] matches = {match1};
-
-        //Tournament tournament = new GroupTournament("Tournament 1",teams,matches);
     }
 }
